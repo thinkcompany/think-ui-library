8a5f1ed0a070966434e1d025e8cff753ca261281
Kathleen Mullins <kathleen.mullins@thinkbrownstone.com>
2020-07-30 14:24:45 -0400

    linting and formatting

--- src/components/Navigation/navigation.js
+++ src/components/Navigation/navigation.js
@@ -1,53 +1,45 @@
 class Nav {
-    constructor() {
-      this.$mobileMenuBtn;
-      this.$mobileMenuTitle;
-      this.$siteNav;
-      this.$body;
-    }
-  
-    setupEventHandler() {
-      const openNavText = 'Open Mobile Navigation';
-      const closeNavText = 'Close Mobile Navigation';
-      this.$mobileMenuBtn[0].addEventListener(
-        'click',
-        () => {
-          let currentText = this.$mobileMenuTitle[0].textContent;
-  
-          this.$mobileMenuBtn[0].classList.toggle('open');
-          this.$mobileMenuBtn[0].classList.toggle('close-icon');
-          this.$mobileMenuBtn[0].classList.toggle('mobile-menu-icon');
-  
-          this.$siteNav[0].classList.toggle('shift');
-          this.$body[0].classList.toggle('shift');
-  
-          currentText === openNavText
-            ? (this.$mobileMenuTitle[0].textContent = closeNavText)
-            : (this.$mobileMenuTitle[0].textContent = openNavText);
-        },
-        false
-      );
-    }
-  
-    init() {
-      document.addEventListener('DOMContentLoaded', () => {
-        this.$mobileMenuBtn = document.querySelectorAll(
-          '[data-mobile-menu-button]'
-        );
-        this.$mobileMenuTitle = document.querySelectorAll(
-          '[data-mobile-menu-title]'
-        );
-  
-        this.$siteNav = document.querySelectorAll(
-          '.menu-site-nav'
-        );
-  
-        this.$body = document.getElementsByTagName('body');
-        
-        this.setupEventHandler();
-      });
-    }
+  constructor() {
+    this.$mobileMenuBtn;
+    this.$mobileMenuTitle;
+    this.$siteNav;
+    this.$body;
   }
-  
-  export { Nav };
-  
+
+  setupEventHandler() {
+    const openNavText = 'Open Mobile Navigation';
+    const closeNavText = 'Close Mobile Navigation';
+    this.$mobileMenuBtn[0].addEventListener(
+      'click',
+      () => {
+        let currentText = this.$mobileMenuTitle[0].textContent;
+
+        this.$mobileMenuBtn[0].classList.toggle('menu-site-nav--open');
+
+        currentText === openNavText
+          ? (this.$mobileMenuTitle[0].textContent = closeNavText)
+          : (this.$mobileMenuTitle[0].textContent = openNavText);
+      },
+      false
+    );
+  }
+
+  init() {
+    document.addEventListener('DOMContentLoaded', () => {
+      this.$mobileMenuBtn = document.querySelectorAll(
+        '[data-mobile-menu-button]'
+      );
+      this.$mobileMenuTitle = document.querySelectorAll(
+        '[data-mobile-menu-title]'
+      );
+
+      this.$siteNav = document.querySelectorAll('.menu-site-nav');
+
+      this.$body = document.getElementsByTagName('body');
+
+      this.setupEventHandler();
+    });
+  }
+}
+
+export { Nav };
--- src/components/Navigation/navigation.stories.js
+++ src/components/Navigation/navigation.stories.js
@@ -2,5 +2,5 @@ import navMarkup from './navigation.html';
 
 export const header = () => navMarkup;
 export default {
-    title: 'Navigation / Global'
-  };
+  title: 'Navigation / Global'
+};
