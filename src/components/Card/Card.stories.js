84289fc8beab016227559ff1a5640c12a587699d
Ian McPhail <ian.mcphail@thinkcompany.com>
2020-08-04 11:29:20 -0400

    Create Small CTA Card

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -1,4 +1,4 @@
-import { select, text } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 
 export const summaryCard = () => {
   const heading = text('Heading', 'Full Experience & Service Design');
@@ -26,8 +26,8 @@ export const summaryCard = () => {
 };
 
 export const callToActionCard = () => {
-  const summary = text(
-    'Summary',
+  const description = text(
+    'Description',
     "Interested in working with Think Company? We scope projects and build teams to meet your organization's unique needs. Tell us about your project today to start the conversation!"
   );
   const cta = text('Call to Action', 'Contact us');
@@ -35,7 +35,7 @@ export const callToActionCard = () => {
   return `
   <div class="tco-card tco-card--cta">
     <div class="tco-card-content-container">
-      <p class="tco-card-content-description">${summary}</p>
+      <p class="tco-card-content-description">${description}</p>
       
     <button type="button" class="btn btn--secondary btn--large">
     ${cta}
@@ -48,7 +48,7 @@ export const personCard = () => {
   const name = text('Name', 'Abby DePrimo');
   const title = text('Title', 'Vice President, Design Operations');
   const image = text(
-    'Image',
+    'Person image',
     'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg'
   );
 
@@ -61,6 +61,43 @@ export const personCard = () => {
       </div>
     </a>
   </div>`;
+};
+
+export const smallCallToAction = () => {
+  const cta = text(
+    'Small Call to Action',
+    'Learn more about working at Think Company'
+  );
+
+  return `
+  <div class="tco-card tco-card--cta-small">
+    <a href="#" class="tco-card-link">
+      <div class="tco-card-content-container">
+        <p class="tco-card-content-description">${cta}</p>
+      </div>
+    </a>
+  </div>`;
+};
+
+export const all = () => {
+  const itemWrapper = children => `
+    <div
+      class="documentation-grid--item"
+      style="text-align:center; margin: 0 0 20px 40px; padding: 20px 0; flex: 0 0 auto"
+    >
+      ${children}
+    </div>`;
+
+  const variations = [
+    summaryCard,
+    callToActionCard,
+    personCard,
+    smallCallToAction
+  ];
+
+  const examples = variations.map(variation => itemWrapper(variation()));
+
+  return `<div class="documentation-grid">${examples.join('')}</div>`;
 };
 
 export default {
