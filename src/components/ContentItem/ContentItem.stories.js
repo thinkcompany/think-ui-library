dcde0902492173df576f22536a3043f75e41f3d5
Christine Martino <christine.martino@thinkcompany.com>
2020-08-26 11:44:13 -0400

    Add initial styles for content item

--- src/components/ContentItem/ContentItem.stories.js
+++ src/components/ContentItem/ContentItem.stories.js
@@ -1,33 +1,56 @@
-import { select, text } from '@storybook/addon-knobs';
+import { text } from '@storybook/addon-knobs';
 
 const contentItem = () => {
   const title = text('Title', 'Become a Digital Disruptor in a Changing World');
   const category = text('Category', 'Workshop');
+  const imageSrc = text('Image Src', 'https://placekitten.com/800/400');
   const excerpt = text(
     'Excerpt',
     'Learn more about best practices for achieving internal alignment and framing conversations for the best possible outcome.'
   );
-  const imageSrc = text('Image Src', 'https://placekitten.com/800/400');
+  const authors = [
+    ' Kiefer William Frederick Dempsey George Rufus Sutherland',
+    ' Benicio Monserrate Rafael del Toro Sánchez',
+    ' Julia Scarlett Elizabeth Louis-Dreyfus'
+  ];
+  const displayAuthors = () =>
+    authors.map(author => `<strong>${author}</strong>`);
 
-  return ` 
-<article class="tco-content-item">
-  <div class="tco-content-item-image">
-    <img class="tco-card-image" alt="Card image" src="${imageSrc}" />
-  </div>
-  <header class="tco-layout-header">
-    <div class="tco-content-item-meta">
-        ${category}
+  return `
+  <article class="tco-content-item">
+  <div class="tco-content-column">
+    <div class="tco-content-item-image">
+      <img class="tco-card-image" alt="Card image" src="${imageSrc}" />
     </div>
-    <h3 class="tco-heading">
-      <a href="#">
-          ${title}
-      </a>
-    </h3>
-  </header>
-  <div class="tco-content-item-excerpt">
-      ${excerpt}
   </div>
-</article>`;
+    <div class="tco-content-column">
+      <header class="tco-content-item-header">
+        <div class="tco-content-item-meta">
+            ${category}
+        </div>
+        <h3 class="tco-heading">
+          <a href="#">
+              ${title}
+          </a>
+        </h3>
+        ${
+          // If excerpt exists, display it
+          excerpt
+            ? `<div class="tco-content-item-excerpt">
+                <p>${excerpt}</p>
+              </div>`
+            : ''
+        }
+        <div class="tco-content-item-authors">
+          <p>Hosted by</p>
+          <p>${displayAuthors()}</p>
+        </div>
+      </header>
+      <footer class="tco-content-item-footer">
+        <button class="tco-btn tco-btn--primary">Read more</button>
+      </footer>
+    </div>
+  </article>`;
 };
 
 export { contentItem };
