d21b14a7866c5100553756b76b00734fdfd10732
Christine Martino <christine.martino@thinkcompany.com>
2020-08-27 12:51:52 -0400

    Add list story

--- src/components/ContentItem/ContentItem.stories.js
+++ src/components/ContentItem/ContentItem.stories.js
@@ -64,6 +64,7 @@ export const Default = () => {
     </div>
   </article>`;
 };
+
 export const Featured = () => {
   const title = text('Title', 'Become a Digital Disruptor in a Changing World');
   const category = text('Category', 'Workshop');
@@ -120,6 +121,61 @@ export const Featured = () => {
       </footer>
     </div>
   </article>`;
+};
+
+export const List = () => {
+  const title = 'Become a Digital Disruptor in a Changing World';
+  const category = 'Workshop';
+  const imageSrc = 'https://placekitten.com/325/245';
+  const excerpt =
+    'Learn more about best practices for achieving internal alignment and framing conversations for the best possible outcome.';
+  const authors = array('Authors', authorSelection);
+  const displayAuthors = () => {
+    const list = authors.map(author => `<strong>${author}</strong>`);
+
+    return list.join(', ');
+  };
+
+  const article = `
+  <article class="tco-content-item">
+  <div class="tco-content-column tco-content-column-media">
+    <div class="tco-content-item-image">
+      <img alt="Card image" src="${imageSrc}" />
+    </div>
+  </div>
+    <div class="tco-content-column tco-content-column-body">
+      <header class="tco-content-item-header">
+        <div class="tco-content-item-meta">
+          <p class="tco-content-category"><span class="tco-tag">${category}</span></p>
+          <div class="tco-content-item-date">
+            <span class="tco-type-body">June 2, 2020</span> &bull; <span class="tco-type-body">Malvern, PA</span>
+          </div>
+        </div>
+        <h3 class="tco-heading">
+          <a href="#" class="tco-link">
+              ${title}
+          </a>
+        </h3>
+        ${
+          // If excerpt exists, display it
+          excerpt
+            ? `<div class="tco-content-item-excerpt">
+                <p class="tco-type-body">${excerpt}</p>
+              </div>`
+            : ''
+        }
+        <div class="tco-content-byline">
+          <p class="tco-type-body tco-content-hosts">Hosted by</p>
+          <p class="tco-type-body tco-content-authors">${displayAuthors()}</p>
+        </div>
+      </header>
+      <footer class="tco-content-item-footer">
+        <button class="tco-btn tco-btn--primary">Read more</button>
+      </footer>
+    </div>
+  </article>`;
+
+  return `${article}${article}${article}`;
 };
 
 export default {
