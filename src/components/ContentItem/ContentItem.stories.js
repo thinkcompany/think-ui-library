484500e24d30194b713c1bd47d25727461c1ec69
Christine Martino <christine.martino@thinkcompany.com>
2020-08-28 15:18:25 -0400

    Add content items data in separate file

--- src/components/ContentItem/ContentItem.stories.js
+++ src/components/ContentItem/ContentItem.stories.js
@@ -1,61 +1,44 @@
 import { array, text, boolean } from '@storybook/addon-knobs';
+import data from './content-item.json';
 
-const authorSelection = [
-  ' Kiefer William Frederick Dempsey George Rufus Sutherland',
-  ' Benicio Monserrate Rafael del Toro Sánchez',
-  ' Julia Scarlett Elizabeth Louis-Dreyfus'
-];
+const { title, authors, category, imageSrc, excerpt } = data[0];
 
-export const Default = () => {
-  const title = text('Title', 'Become a Digital Disruptor in a Changing World');
-  const category = text('Category', 'Workshop');
-  const imageSrc = text('Image Src', 'https://placekitten.com/325/245');
-  const excerpt = text(
-    'Excerpt',
-    'Learn more about best practices for achieving internal alignment and framing conversations for the best possible outcome.'
-  );
-  const authors = array('Authors', authorSelection);
-
-  const mediaLeft = boolean('Media on Left?', false);
-  const displayAuthors = () => {
-    const list = authors.map(author => `<strong>${author}</strong>`);
-
-    return list.join(', ');
-  };
-
+const template = item => {
   return `
   <article class="tco-content-item ${
-    mediaLeft ? 'tco-content-item--media-left' : ''
-  }">
+    item.mediaLeft ? 'tco-content-item--media-left' : ''
+  } ${item.featured ? 'tco-content-item--featured' : ''}">
   <div class="tco-content-column tco-content-column-media">
     <div class="tco-content-item-image">
-      <img alt="Card image" src="${imageSrc}" />
+      <img alt="Card image" src="${item.imageSrc}" />
     </div>
   </div>
     <div class="tco-content-column tco-content-column-body">
       <header class="tco-content-item-header">
         <div class="tco-content-item-meta">
-          <p class="tco-content-category"><span class="tco-tag">${category}</span></p>
+          <p class="tco-content-category"><span class="tco-tag">${
+            item.category
+          }</span></p>
           <div class="tco-content-item-date">
             <span class="tco-type-body">June 2, 2020</span> &bull; <span class="tco-type-body">Malvern, PA</span>
           </div>
         </div>
         <h3 class="tco-heading">
           <a href="#" class="tco-link">
-              ${title}
+              ${item.title}
           </a>
         </h3>
         ${
           // If excerpt exists, display it
-          excerpt
+          item.excerpt
             ? `<div class="tco-content-item-excerpt">
-                <p class="tco-type-body">${excerpt}</p>
+                <p class="tco-type-body">${item.excerpt}</p>
               </div>`
             : ''
         }
         <div class="tco-content-byline">
           <p class="tco-type-body tco-content-hosts">Hosted by</p>
-          <p class="tco-type-body tco-content-authors">${displayAuthors()}</p>
+          <p class="tco-type-body tco-content-authors">${item.authors}</p>
         </div>
       </header>
       <footer class="tco-content-item-footer">
@@ -63,119 +46,69 @@ export const Default = () => {
       </footer>
     </div>
   </article>`;
+};
+
+export const Default = () => {
+  const categoryKnob = text('Category', category);
+  const imageSrcKnob = text('Image Src', imageSrc);
+  const excerptKnob = text('Excerpt', excerpt);
+  const authorKnob = array('Authors', authors);
+  const mediaLeft = boolean('Media on Left?', false);
+  const displayAuthors = () => {
+    const list = authorKnob.map(author => `<strong>${author}</strong>`);
+
+    return list.join(', ');
+  };
+
+  return template({
+    title: text('Title', title),
+    category: categoryKnob,
+    imageSrc: imageSrcKnob,
+    excerpt: excerptKnob,
+    mediaLeft: mediaLeft,
+    authors: displayAuthors()
+  });
 };
 
 export const Featured = () => {
-  const title = text('Title', 'Become a Digital Disruptor in a Changing World');
-  const category = text('Category', 'Workshop');
-  const imageSrc = text('Image Src', 'https://placekitten.com/325/245');
-  const excerpt = text(
-    'Excerpt',
-    'Learn more about best practices for achieving internal alignment and framing conversations for the best possible outcome.'
-  );
+  const categoryKnob = text('Category', category);
+  const imageSrcKnob = text('Image Src', imageSrc);
+  const excerptKnob = text('Excerpt', excerpt);
+  const authorKnob = array('Authors', authors);
   const mediaLeft = boolean('Media on Left?', false);
-  const authors = array('Authors', authorSelection);
   const displayAuthors = () => {
-    const list = authors.map(author => `<strong>${author}</strong>`);
+    const list = authorKnob.map(author => `<strong>${author}</strong>`);
 
     return list.join(', ');
   };
 
-  return `
-  <article class="tco-content-item tco-content-item--featured ${
-    mediaLeft ? 'tco-content-item--media-left' : ''
-  }">
-  <div class="tco-content-column tco-content-column-media">
-    <div class="tco-content-item-image">
-      <img alt="Card image" src="${imageSrc}" />
-    </div>
-  </div>
-    <div class="tco-content-column tco-content-column-body">
-      <header class="tco-content-item-header">
-        <div class="tco-content-item-meta">
-          <p class="tco-content-category"><span class="tco-tag">${category}</span></p>
-          <div class="tco-content-item-date">
-            <span class="tco-type-body">June 2, 2020</span> &bull; <span class="tco-type-body">Malvern, PA</span>
-          </div>
-        </div>
-        <h3 class="tco-heading">
-          <a href="#" class="tco-link">
-              ${title}
-          </a>
-        </h3>
-        ${
-          // If excerpt exists, display it
-          excerpt
-            ? `<div class="tco-content-item-excerpt">
-                <p class="tco-type-body">${excerpt}</p>
-              </div>`
-            : ''
-        }
-        <div class="tco-content-byline">
-          <p class="tco-type-body tco-content-hosts">Hosted by</p>
-          <p class="tco-type-body tco-content-authors">${displayAuthors()}</p>
-        </div>
-      </header>
-      <footer class="tco-content-item-footer">
-        <button class="tco-btn tco-btn--primary">Read more</button>
-      </footer>
-    </div>
-  </article>`;
+  return template({
+    title: text('Title', title),
+    category: categoryKnob,
+    imageSrc: imageSrcKnob,
+    excerpt: excerptKnob,
+    mediaLeft: mediaLeft,
+    featured: true,
+    authors: displayAuthors()
+  });
 };
 
 export const List = () => {
-  const title = 'Become a Digital Disruptor in a Changing World';
-  const category = 'Workshop';
-  const imageSrc = 'https://placekitten.com/325/245';
-  const excerpt =
-    'Learn more about best practices for achieving internal alignment and framing conversations for the best possible outcome.';
-  const authors = array('Authors', authorSelection);
-  const displayAuthors = () => {
-    const list = authors.map(author => `<strong>${author}</strong>`);
+  const output = data.map(item => {
+    const displayAuthors = () =>
+      item.authors.map(author => `<strong>${author}</strong>`).join('');
 
-    return list.join(', ');
-  };
+    return template({
+      title: item.title,
+      category: item.category,
+      imageSrc: item.imageSrc,
+      excerpt: item.excerpt,
+      mediaLeft: false,
+      authors: displayAuthors()
+    });
+  });
 
-  const article = `
-  <article class="tco-content-item">
-  <div class="tco-content-column tco-content-column-media">
-    <div class="tco-content-item-image">
-      <img alt="Card image" src="${imageSrc}" />
-    </div>
-  </div>
-    <div class="tco-content-column tco-content-column-body">
-      <header class="tco-content-item-header">
-        <div class="tco-content-item-meta">
-          <p class="tco-content-category"><span class="tco-tag">${category}</span></p>
-          <div class="tco-content-item-date">
-            <span class="tco-type-body">June 2, 2020</span> &bull; <span class="tco-type-body">Malvern, PA</span>
-          </div>
-        </div>
-        <h3 class="tco-heading">
-          <a href="#" class="tco-link">
-              ${title}
-          </a>
-        </h3>
-        ${
-          // If excerpt exists, display it
-          excerpt
-            ? `<div class="tco-content-item-excerpt">
-                <p class="tco-type-body">${excerpt}</p>
-              </div>`
-            : ''
-        }
-        <div class="tco-content-byline">
-          <p class="tco-type-body tco-content-hosts">Hosted by</p>
-          <p class="tco-type-body tco-content-authors">${displayAuthors()}</p>
-        </div>
-      </header>
-      <footer class="tco-content-item-footer">
-        <button class="tco-btn tco-btn--primary">Read more</button>
-      </footer>
-    </div>
-  </article>`;
-
-  return `${article}${article}${article}`;
+  return output.join('');
 };
 
 export default {
