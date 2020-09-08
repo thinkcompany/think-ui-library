c5a934cf89fba3edf23c0cc3f2db5066605b2ae6
Christine Martino <christine.martino@thinkcompany.com>
2020-09-08 09:23:14 -0400

    Feature: Image Grid (#25)
    * Add Image Grid Styles
    
    * Add image count knob
    
    * Add bordered version (mobile)
    
    * Split complex logic into a mixin
    
    * Apply correct styles to caption
    
    * Remove unknown property
    
    * linting & formatting
    
    Co-authored-by: Kathleen Mullins <kathleen.mullins@thinkcompany.com>

---
+++ src/components/ImageGrid/ImageGrid.stories.js
@@ -0,0 +1,59 @@
+import { number } from '@storybook/addon-knobs';
+
+const imageItem = () => {
+  const image =
+    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/themes/tbiv3/img/svgs/award-inc5000.svg';
+  const caption = '7 YEARS ON THE LIST';
+
+  return `
+    <div class="tco-image-grid-container">
+      <img class="tco-image-grid-media" alt="Client image" src="${image}" />
+      ${
+        caption
+          ? '<span class="tco-image-grid-caption">' + caption + '</span>'
+          : ''
+      }
+    </div>`;
+};
+
+export const Main = () => {
+  const imageItems = () => {
+    const imageCount = number('Image count', 4);
+
+    let output = '';
+
+    for (let index = 0; index < imageCount; index++) {
+      output += imageItem();
+    }
+
+    return output;
+  };
+
+  return `
+  <div class="tco-image-grid">
+    ${imageItems()}
+  </div>`;
+};
+
+export const withBorder = () => {
+  const imageItems = () => {
+    const imageCount = number('Image count', 15);
+
+    let output = '';
+
+    for (let index = 0; index < imageCount; index++) {
+      output += imageItem();
+    }
+
+    return output;
+  };
+
+  return `
+  <div class="tco-image-grid tco-image-grid--border">
+    ${imageItems()}
+  </div>`;
+};
+
+export default {
+  title: 'Components / Image Grid'
+};
