0b82b760e362c6e52b49525a1abef472656c05f2
Nicole Witte <nicole.witte@thinkbrownstone.com>
2020-10-05 10:55:31 -0400

    fix grid spacing issue w/ flex alignment

--- src/components/Author/Author.stories.js
+++ src/components/Author/Author.stories.js
@@ -14,27 +14,6 @@ const colors = {
   Glass: 'tco-container-wrapper--glass'
 };
 
-const summaryCard = () => {
-  const heading = 'Full Experience & Service Design';
-  const summary =
-    'The best experiences are consistent across all channels, so we always consider the entire ecosystem your users interact within—designing for people wherever they click, tap, talk, touch, work, or play.';
-  const image =
-    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/themes/tbiv3/img/svgs/services-full-experience-service.svg';
-
-  return `
-  <div class="tco-card tco-card--summary">
-    <a href="#" class="tco-card-link">
-      <div class="tco-card-image-container">
-        <img class="tco-card-image" alt="An adorable kitten" src="${image}" />
-      </div>
-      <div class="tco-card-content-container">
-        <h3 class="tco-card-content-heading">${heading}</h3>
-        <p class="tco-card-content-description">${summary}</p>
-      </div>
-    </a>
-  </div>`;
-};
-
 export const Default = () => {
   const containerSize = select('Size', sizes, sizes.Default);
   const containerColor = select('State', colors, colors.Default);
@@ -50,19 +29,6 @@ export const Default = () => {
 
 export const Mid = () => {
   const containerSize = select('Size', sizes, sizes.Mid);
-  const containerColor = select('State', colors, colors.Navy);
-
-  return `
-  <div class="tco-container-wrapper ${containerColor}">
-    <div class="tco-container ${containerSize}"> 
-      <p>I'm accessible text!</p>
-      <a href="#">I'm an accessible link!</a>
-    </div>
-  </div>`;
-};
-
-export const Narrow = () => {
-  const containerSize = select('Size', sizes, sizes.Narrow);
   const containerColor = select('State', colors, colors.Navy);
 
   return `
@@ -113,13 +79,36 @@ export const Blue = () => {
   </div>`;
 };
 
-export const WithComponent = () => {
-  const containerSize = select('Size', sizes, sizes.Default);
+export const Narrow = () => {
+  const containerSize = select('Size', sizes, sizes.Narrow);
   const containerColor = select('State', colors, colors.Navy);
 
   return `
+  <div class="tco-container-wrapper ${containerColor}">
+    <div class="tco-container ${containerSize}"> 
+      <p>I'm accessible text!</p>
+      <a href="#">I'm an accessible link!</a>
+    </div>
+  </div>`;
+};
+
+export const WithComponent = () => {
+  const containerSize = select('Size', sizes, sizes.Default);
+  const containerColor = select('State', colors, colors.Tan);
+
+  return `
   <div class="tco-container ${containerSize} ${containerColor}">
-    ${summaryCard()}
+  <div class="tco-card tco-card--summary">
+  <a href="#" class="tco-card-link">
+    <div class="tco-card-image-container">
+      <img class="tco-card-image" alt="Card image" src="https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/themes/tbiv3/img/svgs/services-full-experience-service.svg" />
+    </div>
+    <div class="tco-card-content-container">
+      <h3 class="tco-card-content-heading">Heading!</h3>
+      <p class="tco-card-content-description"> lorem ipsum text </p>
+    </div>
+  </a>
+</div>
   </div>`;
 };
 
