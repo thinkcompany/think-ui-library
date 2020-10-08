ac2a539df35b2b11e8bbaca90042833abc5f440e
think-nicolewitte <42655717+think-nicolewitte@users.noreply.github.com>
2020-10-08 10:37:16 -0400

    Fix/tco 419 (#29)
    * fix grid spacing issue w/ flex alignment
    
    * fix text color on navy background
    
    * forgot to run linters
    
    * remove author component

--- src/components/CardGrid/CardGrid.stories.js
+++ src/components/CardGrid/CardGrid.stories.js
@@ -21,6 +21,27 @@ const summaryCard = () => {
   </div>`;
 };
 
+const summaryCardSmall = () => {
+  const heading = 'Full Experience & Service Design';
+  const summary =
+    'The your users interact within—designing for people wherever they click, tap, talk, touch, work, or play.';
+  const image =
+    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/themes/tbiv3/img/svgs/services-full-experience-service.svg';
+
+  return `
+  <div class="tco-card tco-card--summary">
+    <a href="#" class="tco-card-link">
+      <div class="tco-card-image-container">
+        <img class="tco-card-image" alt="An adorable kitten" src="${image}" />
+      </div>
+      <div class="tco-card-content-container">
+        <h3 class="tco-card-content-heading">${heading}</h3>
+        <p class="tco-card-content-description">${summary}</p>
+      </div>
+    </a>
+  </div>`;
+};
+
 const postCard = size => {
   const image =
     'https://thinkcompany.com/wp-content/uploads/2015/12/ss-internetprovider-mobile-top.jpg';
@@ -90,6 +111,9 @@ export const threeColumn = () => {
   return `
   <div class="tco-card-grid tco-card-grid--${columns}-column">
     ${summaryCard()}
+    ${summaryCardSmall()}
+    ${summaryCard()}
+    ${summaryCardSmall()}
     ${summaryCard()}
     ${summaryCard()}
   </div>`;
--- src/components/Container/Container.stories.js
+++ src/components/Container/Container.stories.js
@@ -14,6 +14,29 @@ const colors = {
   Glass: 'tco-container-wrapper--glass'
 };
 
+const summaryCardWithGrid = () => {
+  const heading = 'Full Experience & Service Design';
+  const summary =
+    'The best experiences are consistent across all channels, so we always consider the entire ecosystem your users interact within—designing for people wherever they click, tap, talk, touch, work, or play.';
+  const image =
+    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/themes/tbiv3/img/svgs/services-full-experience-service.svg';
+
+  return `
+  <div class="tco-card-grid tco-card-grid--3-column">
+    <div class="tco-card tco-card--summary">
+      <a href="#" class="tco-card-link">
+        <div class="tco-card-image-container">
+          <img class="tco-card-image" alt="An adorable kitten" src="${image}" />
+        </div>
+        <div class="tco-card-content-container">
+          <h3 class="tco-card-content-heading">${heading}</h3>
+          <p class="tco-card-content-description">${summary}</p>
+        </div>
+      </a>
+    </div>
+  </div>`;
+};
+
 export const Default = () => {
   const containerSize = select('Size', sizes, sizes.Default);
   const containerColor = select('State', colors, colors.Default);
@@ -94,22 +117,13 @@ export const Blue = () => {
 
 export const WithComponent = () => {
   const containerSize = select('Size', sizes, sizes.Default);
-  const containerColor = select('State', colors, colors.Tan);
+  const containerColor = select('State', colors, colors.Navy);
 
   return `
   <div class="tco-container ${containerSize} ${containerColor}">
-  <div class="tco-card tco-card--summary">
-  <a href="#" class="tco-card-link">
-    <div class="tco-card-image-container">
-      <img class="tco-card-image" alt="Card image" src="https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/themes/tbiv3/img/svgs/services-full-experience-service.svg" />
-    </div>
-    <div class="tco-card-content-container">
-      <h3 class="tco-card-content-heading">Heading!</h3>
-      <p class="tco-card-content-description"> lorem ipsum text </p>
-    </div>
-  </a>
-</div>
-  </div>`;
+    ${summaryCardWithGrid()}
+  </div>
+  `;
 };
 
 export default {
