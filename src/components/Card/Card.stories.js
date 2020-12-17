1e0af150b9a962419c13e6c4dd7e9f3091be3dd1
Andy Knapp <andy.knapp@thinkcompany.com>
2020-12-17 09:52:28 -0500

    Tco 478 post grid bugs (#35)
    * card bugs
    
    * card style adjustments
    
    * add spacing around img container
    
    * remove space around img in card-post

--- src/components/Card/Card.stories.js
+++ src/components/Card/Card.stories.js
@@ -16,7 +16,6 @@ export const summaryCard = () => {
   return `
   <div class="tco-card tco-card--summary">
     <a href="#" class="tco-card-link">
-    </a>
       <div class="tco-card-image-container">
         <img class="tco-card-image" alt="Card image" src="${image}" />
       </div>
@@ -24,7 +23,7 @@ export const summaryCard = () => {
         <h3 class="tco-card-content-heading">${heading}</h3>
         <p class="tco-card-content-description">${summary}</p>
       </div>
-    
+    </a>
   </div>`;
 };
 
@@ -39,7 +38,7 @@ export const callToActionCard = () => {
   <div class="tco-card tco-card--cta">
     <div class="tco-card-content-container">
       <p class="tco-card-content-description">${description}</p>
-      
+
     <button type="button" class="tco-btn tco-btn--secondary tco-btn--large">
     ${cta}
   </button>
