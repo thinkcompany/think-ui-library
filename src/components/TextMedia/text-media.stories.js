90a79f851b5eb4cfc7eb33b0ca6eaa043ce4c13b
Andy <andy.knapp@thinkcompany.com>
2021-01-25 16:38:04 -0500

    linting error

--- src/components/TextMedia/text-media.stories.js
+++ src/components/TextMedia/text-media.stories.js
@@ -1,93 +1,162 @@
 import { select, text, boolean } from '@storybook/addon-knobs';
+import { teaser } from '../Teaser/teaser.stories';
 
-const backgrounds = ['white', 'tan', 'glass', 'blue', 'navy'];
+const colors = {
+  Default: '',
+  Tan: 'tco-container-wrapper--tan',
+  Blue: 'tco-container-wrapper--blue',
+  Navy: 'tco-container-wrapper--navy',
+  Glass: 'tco-container-wrapper--glass'
+};
 const alignments = ['center', 'left', 'right'];
+const sizes = ['extra-large', 'large', 'medium', 'small'];
 
-export const centered = () => {
-  const background = select('Background', backgrounds, 'white');
-  const alignment = select('Text Alignment', alignments, 'left');
+export const Default = () => {
+  const containerColor = select('Background Color', colors, colors.Glass);
+  const alignment = select('Media Alignment', alignments, 'left');
+  const image = text('Image', 'https://placekitten.com/524/320');
+  //const image = text('Image', 'https://placekitten.com/390/524');
+  const imageBackground = boolean('Image background?', true);
+  const textAlignment = select('Text Alignment', alignments, 'left');
   const eyebrow = text('Eyebrow', 'Text and Image');
   const heading = text('Heading', 'Technology Integration');
+  const headingSize = select('Heading Size', sizes, 'small');
   const lede = text(
     'Lede',
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque nunc vel turpis tempor tincidunt. Maecenas in felis suscipit, ornare felis at, posuere eros.'
   );
-  const image = text('Image', 'https://placekitten.com/800/400');
-  const cta = boolean('CTA', true);
+  const showCta = boolean('CTA', true);
+  const showTeaser = boolean('Show Teaser', false);
+  const numbered = boolean('Numbered', false);
+
+  // get media aspect ratio for background bubble style
+  const url = new URL(image);
+  const path = url.pathname;
+  const size = path.split('/');
+  const aspectRatio = size[1] / size[2];
+  let mediaOrientation;
+
+  if (aspectRatio > 1) {
+    mediaOrientation = 'wide';
+  } else {
+    mediaOrientation = 'tall';
+  }
 
   return /*html*/ `
-  <div class="tco-container-wrapper tco-container-wrapper--${background}">
+  <div class="tco-container-wrapper ${containerColor}">
     <div class="tco-container tco-container--default">
       <div class="tco-text-media tco-text-media--align-${alignment}">
-        <div class="tco-text-media-content tco-text-media-content--text">
-          <span class="tco-text-media-eyebrow">${eyebrow}</span>
-          <h1 class="tco-text-media-heading">${heading}</h1>
+        <div class="tco-text-media-content tco-text-media-content--text tco-text-media-content--text-${textAlignment}">
+          ${
+            numbered
+              ? `
+            <span class="tco-text-media--numbered">1</span>
+          `
+              : ''
+          }
+          ${
+            eyebrow
+              ? `
+            <span class="tco-text-media-eyebrow">${eyebrow}</span>
+            `
+              : ''
+          }
+
+          <h1 class="tco-text-media-heading tco-type-display--${headingSize}">${heading}</h1>
           <div class="tco-text-media-lede">
             <p>${lede}</p>
           </div>
-          <a href="#" class="tco-btn tco-btn--primary">Learn More</a>
+          ${
+            showCta
+              ? `
+            <a href="#" class="tco-btn tco-btn--primary">Learn More</a>
+          `
+              : ''
+          }
+          ${showTeaser ? teaser() : ''}
         </div>
+        <div class="tco-text-media-content tco-text-media-content--media tco-text-media-content--media-${mediaOrientation} ${
+    imageBackground ? 'tco-text-media-content--media-background' : ''
+  }">
+          ${
+            imageBackground
+              ? `
+            <div class="tco-text-media-container tco-text-media-container--${mediaOrientation}">
+              <img class="tco-text-media-image" alt="An adorable kitten" src="${image}" />
+            </div>
+          `
+              : `
+            <img class="tco-text-media-image" alt="An adorable kitten" src="${image}" />
+          `
+          }
+
+        </div>
+      </div>
+    </div>
+  </div>`;
+};
+
+export const PageHeader = () => {
+  const containerColor = select('Background Color', colors, colors.Default);
+  const image = text('Image', 'https://placekitten.com/524/524');
+  const circleBackground = boolean('Circle background?', false);
+  const textAlignment = select('Text Alignment', alignments, 'left');
+  const eyebrow = text('Eyebrow', '');
+  const heading = text(
+    'Heading',
+    'Adapting your digital experience is critical'
+  );
+  const headingSize = select('Heading Size', sizes, 'medium');
+  const lede = text(
+    'Lede',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas pellentesque nunc vel turpis tempor tincidunt at, posuere eros.'
+  );
+  const showCta = boolean('CTA', true);
+
+  // // get media aspect ratio for background bubble style
+  // const url = new URL(image);
+  // const path = url.pathname;
+  // const size = path.split('/');
+  // const aspectRatio = size[1] / size[2];
+  // let mediaOrientation;
+  //
+  // if( aspectRatio > 1) {
+  //   mediaOrientation = 'wide';
+  // } else {
+  //   mediaOrientation = 'tall';
+  // }
+
+  return /*html*/ `
+  <div class="tco-container-wrapper ${containerColor} tco-text-media--page-header">
+    <div class="tco-container tco-container--default">
+      <div class="tco-text-media tco-text-media--align-left ${
+        circleBackground ? 'tco-text-media-content--media-background' : ''
+      }">
+        <div class="tco-text-media-content tco-text-media-content--text tco-text-media-content--text-${textAlignment}">
+          ${
+            eyebrow
+              ? `
+            <span class="tco-text-media-eyebrow">${eyebrow}</span>
+            `
+              : ''
+          }
+
+          <h1 class="tco-text-media-heading tco-type-display--${headingSize}">${heading}</h1>
+          <div class="tco-text-media-lede">
+            <p>${lede}</p>
+          </div>
+          ${
+            showCta
+              ? `
+            <a href="#" class="tco-btn tco-btn--primary">Learn More</a>
+          `
+              : ''
+          }
+        </div>
+
         <div class="tco-text-media-content tco-text-media-content--media">
           <img class="tco-text-media-image" alt="An adorable kitten" src="${image}" />
         </div>
-      </div>
-    </div>
-  </div>`;
-};
-
-export const textLeft = () => {
-  const background = select('Background', backgrounds, 'white');
-  const eyebrow = text('Eyebrow', 'Eyebrow');
-  const heading = text('Heading', 'Section Title');
-  const lede = text(
-    'Lede',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod augue imperdiet luctus vestibulum. Nulla facilisi. Vivamus tincidunt, libero in ultricies aliquam, urna diam faucibus magna, sed bibendum lectus orci ac magna.'
-  );
-  const image = text('Image', 'https://placekitten.com/580/580');
-  const linkText = text('Link Text', 'Button');
-
-  return /*html*/ `
-  <div class="tco-container-wrapper tco-container-wrapper--${background}">
-  <div class="tco-container">
-  <div class="tco-text-media tco-text-media--align-left">
-      <div class="tco-text-media-content">
-        <span class="tco-text-media-eyebrow">${eyebrow}</span>
-        <h1 class="tco-text-media-heading">${heading}</h1>
-        <div class="tco-text-media-lede">
-          <p>${lede}</p>
-        </div>
-        <a href="#" class="tco-btn tco-btn--primary tco-text-media-cta">${linkText}</a>
-      </div>
-      <img class="tco-text-media-image" alt="An adorable kitten" src="${image}" />
-    </div>
-  </div>
-  </div>`;
-};
-
-export const textRight = () => {
-  const background = select('Background', backgrounds, 'white');
-  const eyebrow = text('Eyebrow', 'Eyebrow');
-  const heading = text('Heading', 'Section Title');
-  const lede = text(
-    'Lede',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent euismod augue imperdiet luctus vestibulum. Nulla facilisi. Vivamus tincidunt, libero in ultricies aliquam, urna diam faucibus magna, sed bibendum lectus orci ac magna.'
-  );
-  const image = text('Image', 'https://placekitten.com/580/580');
-  const linkText = text('Link Text', 'Button');
-
-  return /*html*/ `
-  <div class="tco-container-wrapper tco-container-wrapper--${background}">
-  <div class="tco-container">
-  <div class="tco-text-media tco-text-media--align-right">
-      <div class="tco-text-media-content">
-        <span class="tco-text-media-eyebrow">${eyebrow}</span>
-        <h1 class="tco-text-media-heading">${heading}</h1>
-        <div class="tco-text-media-lede">
-          <p>${lede}</p>
-        </div>
-        <a href="#" class="tco-btn tco-btn--primary tco-text-media-cta">${linkText}</a>
-      </div>
-      <img class="tco-text-media-image" alt="An adorable kitten" src="${image}" />
     </div>
   </div>
   </div>`;
