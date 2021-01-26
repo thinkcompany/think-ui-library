94983f98b89647f498951e9d62098f9910dee51e
Andy <andy.knapp@thinkcompany.com>
2021-01-26 15:42:57 -0500

    text only bubble background

--- src/components/TextOnly/text-only.stories.js
+++ src/components/TextOnly/text-only.stories.js
@@ -9,23 +9,30 @@ const colors = {
 };
 const alignments = ['center', 'left', 'right'];
 const sizes = ['extra-large', 'large', 'medium', 'small'];
+const ledeSizes = ['large', 'default', 'sans-small'];
+const containerSizes = ['default', 'mid', 'narrow', 'none'];
 
 export const Default = () => {
-  const containerColor = select('Background Color', colors, colors.Glass);
+  const containerColor = select('Background Color', colors, colors.Default);
   const textAlignment = select('Text Alignment', alignments, 'left');
   const eyebrow = text('Eyebrow', 'Text Only');
   const heading = text('Heading', 'Technology Integration');
-  const headingSize = select('Heading Size', sizes, 'large');
+  const headingSize = select('Heading Size', sizes, 'medium');
   const lede = text(
     'Lede',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae dui posuere, porta nunc lacinia, pharetra diam. Maecenas dapibus orci vitae accumsan lacinia. Praesent dapibus ac leo ut aliquam. Praesent commodo hendrerit est. Etiam aliquam imperdiet mi, at vulputate sem condimentum quis. Vivamus pellentesque sed velit et pulvinar. </br>Quisque semper erat vestibulum ex congue tempus. Morbi cursus efficitur nulla vitae pellentesque.'
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae dui posuere, porta nunc lacinia, pharetra diam. Maecenas dapibus orci vitae accumsan lacinia. Praesent dapibus ac leo ut aliquam. Praesent commodo hendrerit est.'
   );
+  const ledeSize = select('Lede Size', ledeSizes, 'default');
+  const ledeContainer = select('Lede Container', containerSizes, 'none');
   const showCta = boolean('CTA', true);
+  const bubble = boolean('Bubble Background', false);
 
-  return /*html*/ `
+  return `
   <div class="tco-container-wrapper ${containerColor}">
     <div class="tco-container tco-container--default">
-      <div class="tco-text-only tco-text-only--${textAlignment}">
+      <div class="tco-text-only tco-text-only--${textAlignment} ${
+    bubble ? ` tco-text-only--bubble` : ''
+  }">
         ${
           eyebrow
             ? `
@@ -33,9 +40,15 @@ export const Default = () => {
           `
             : ''
         }
-        <h1 class="tco-text-only-heading tco-type-display--${headingSize}">${heading}</h1>
-        <div class="tco-text-only-content">
-          <p class="tco-text-only-lede">${lede}</p>
+        ${
+          heading
+            ? `
+            <h1 class="tco-text-only-heading tco-type-display--${headingSize}">${heading}</h1>
+          `
+            : ''
+        }
+        <div class="tco-text-only-content tco-text-only-content--${textAlignment} tco-container--${ledeContainer}">
+          <p class="tco-text-only-lede tco-type-body--${ledeSize}">${lede}</p>
         </div>
         ${
           showCta
