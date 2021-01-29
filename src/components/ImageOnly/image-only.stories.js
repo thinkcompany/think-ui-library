693b4c0208f2ae8d57c7c232732d0175f5995fda
Andy Knapp <andy.knapp@thinkcompany.com>
2021-01-29 09:56:19 -0500

    TCR-70: image only component (#55)
    * image only component
    
    * remove comments
    
    * text knob for image, generic alt text, and fewer colors

---
+++ src/components/ImageOnly/image-only.stories.js
@@ -0,0 +1,42 @@
+import { select, text, boolean } from '@storybook/addon-knobs';
+
+const colors = {
+  Default: '',
+  Glass: 'tco-container-wrapper--glass'
+};
+
+const offsets = {
+  Default: '',
+  Top: 'tco-image-only--offset-top',
+  Bottom: 'tco-image-only--offset-bottom'
+};
+
+export const imageOnly = () => {
+  const containerColor = select('Background Color', colors, colors.Glass);
+  const offset = select('Offset Image', offsets, offsets.Default);
+  const imageSrc = text(
+    'Image',
+    'https://thinkstaging.wpengine.com/wp-content/uploads/2021/01/careers-header.jpg'
+  );
+
+  return `
+    <div class="tco-image-only ${offset}">
+      <img class="tco-image-only-image" alt="A brief description of the image" src="${imageSrc}" />
+    </div>`;
+};
+
+export const WithContainer = () => {
+  const containerColor = select('Background Color', colors, colors.Default);
+
+  return `
+    <div class="tco-container-wrapper tco-container-wrapper--image-only ${containerColor}">
+      <div class="tco-container">
+        ${imageOnly()}
+      </div>
+    </div>
+  `;
+};
+
+export default {
+  title: 'Components / Image Only'
+};
