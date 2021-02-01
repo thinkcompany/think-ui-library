1df18332c98a0394cf5993fcd21bf5148d080fa2
Andy <andy.knapp@thinkcompany.com>
2021-02-01 14:08:13 -0500

    updated link styles

--- src/components/Buttons/Buttons.stories.js
+++ src/components/Buttons/Buttons.stories.js
@@ -5,9 +5,30 @@ const styles = {
   Secondary: 'tco-btn--secondary'
 };
 
+const linkStyles = {
+  Primary: '',
+  Secondary: 'tco-text-link--secondary'
+};
+
 const states = {
   Active: '',
   Disabled: 'disabled'
+};
+
+const sizes = {
+  DisplayXLarge: 'display--extra-large',
+  DisplayLarge: 'display--large',
+  DisplayMedium: 'display--medium',
+  DisplaySmall: 'display--small',
+  DisplayXSmall: 'display--extra-small',
+  BodyDefault: 'body--default',
+  BodyLarge: 'body--large',
+  BodySansSmall: 'body--sans-small'
+};
+
+const colors = {
+  Default: '',
+  Glass: 'tco-container-wrapper--glass'
 };
 
 export const Button = () => {
@@ -22,16 +43,40 @@ export const Button = () => {
 };
 
 export const TextLink = () => {
-  const buttonText = text('Text', 'Text Link');
-  const buttonState = select('State', states, states.Active);
+  const linkText = text('Text', 'default text link');
+  const linkState = select('State', states, states.Active);
 
   return `
-    <a href="#" class="tco-btn-link" ${buttonState}>
-      ${buttonText}
-      <svg class="tco-btn-link-icon" width="20" height="20" viewBox="0 0 20 20" role="img" aria-labelledby="arrow">
-        <use xlink:href="/img/icons.svg#icon-arrow-right"></use>
-      </svg>
-    </a>`;
+    <p class="tco-type-body--default">Lorem ipsum dolor sit amet <a href="#" class="tco-text-link" ${linkState}> ${linkText}</a>.</p>
+    <p class="tco-type-body--sans-small">Lorem ipsum <a href="#" class="tco-text-link" ${linkState}> ${linkText}</a> dolor sit amet.</p>`;
+};
+
+export const LinkWithArrow = () => {
+  const linkText = text('Text', 'Text Link with Arrow');
+  const linkStyle = select('Style', linkStyles, linkStyles.Primary);
+  const linkState = select('State', states, states.Active);
+  const fontSize = select('Font Size', sizes, sizes.BodyDefault);
+  let containerColor;
+
+  if (linkStyle == 'tco-text-link--secondary') {
+    containerColor = 'tco-container-wrapper--navy';
+  } else {
+    containerColor = '';
+  }
+
+  return `
+  <div class="tco-container-wrapper ${containerColor}">
+    <div class="tco-container">
+      <h3 class="tco-type-${fontSize}">
+        <a href="#" class="tco-text-link tco-text-link--arrow ${linkStyle}" ${linkState}>
+          ${linkText}
+          <svg class="tco-text-link-icon" width="20" height="20" viewBox="0 0 20 20" role="img" aria-labelledby="arrow">
+            <use xlink:href="/img/icons.svg#icon-arrow-right"></use>
+          </svg>
+        </a>
+      </h3>
+    </div>
+  </div>`;
 };
 
 export default {
