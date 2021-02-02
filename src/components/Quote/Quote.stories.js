fa07132c3146b01383f88165c6eb3a3c99691fc2
Andy <andy.knapp@thinkcompany.com>
2021-02-02 10:08:40 -0500

    update quotation mark

--- src/components/Quote/Quote.stories.js
+++ src/components/Quote/Quote.stories.js
@@ -12,7 +12,7 @@ export const Default = () => {
     'Quote',
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.'
   );
-  const containerColor = select('Background Color', colors, colors.Glass);
+  const containerColor = select('Background Color', colors, colors.Default);
   const textAlignment = select('Text Alignment', alignments, 'center');
   const authorName = text('Name', 'Fake Name');
   const authorTitle = text('Title/Company', 'ACME Company');
@@ -22,7 +22,9 @@ export const Default = () => {
     <div class="tco-container tco-container--mid">
       <div class="tco-quote-wrapper tco-quote-wrapper-${textAlignment}">
         <div class="tco-background-blob tco-background-blob--quote">
-          <span class="tco-background-blob-content tco-background-blob-content--quote">&ldquo;</span>
+          <svg class="tco-background-blob-content tco-background-blob-content--quote" width="18px" height="13px" viewBox="0 0 18 13" role="presentation">
+            <use xlink:href="/img/icons.svg#icon-quotemark"></use>
+          </svg>
           <svg class="tco-background-blob-icon" width="40" viewBox="0 0 40 40" role="img" aria-labelledby="1">
             <use xlink:href="/img/icons.svg#icon-blob-1"></use>
           </svg>
