9707a7604adc28ba896bc71bfe70b98ebf24750e
Andy <andy.knapp@thinkcompany.com>
2021-02-11 13:30:05 -0500

    import byline into post card component. merge master

--- src/components/Byline/byline.stories.js
+++ src/components/Byline/byline.stories.js
@@ -1,11 +1,11 @@
 import { select, boolean, text, number } from '@storybook/addon-knobs';
 
-export const Default = () => {
-  const bylineName = text('Author Name', 'FirstName LastName');
-  const bylineDesc = text('Byline Description', 'July 8, 2020');
+export const byline = () => {
+  const bylineName = text('Post Author', 'Keith Rich');
+  const bylineDesc = text('Post Date', 'May 21, 2020');
   const bylineImage = text(
     'Image',
-    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg'
+    'https://thinkcompany.com/wp-content/uploads/2018/05/KeithRich_Web.jpg'
   );
   const bylineLink = boolean('With link?', false);
 
@@ -22,19 +22,15 @@ export const Default = () => {
   }
 
   return `
-    <div class="tco-container-wrapper tco-author">
-      <div class="tco-container tco-container--default">
-        ${containerOpen}
-          <div class="tco-byline-image-container">
-            <img src="https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg" alt="${bylineName}">
-          </div>
-          <div class="tco-byline-content-container">
-            <p class="tco-byline-name">${bylineName}</p>
-            <p class="tco-byline-description">${bylineDesc}</p>
-          </div>
-        ${containerClose}
+    ${containerOpen}
+      <div class="tco-byline-image-container">
+        <img src="${bylineImage}" alt="${bylineName}">
       </div>
-    </div>
+      <div class="tco-byline-content-container">
+        <p class="tco-byline-name">By ${bylineName}</p>
+        <p class="tco-byline-description">${bylineDesc}</p>
+      </div>
+    ${containerClose}
   `;
 };
 
