3fae0c24753e218b7b86f0bd7adf1e7131d8b936
Andy <andy.knapp@thinkcompany.com>
2021-02-12 16:51:32 -0500

    import components into column layout

---
+++ src/components/Columns/Columns.stories.js
@@ -0,0 +1,28 @@
+import { select, text, boolean, object } from '@storybook/addon-knobs';
+import { textOnly } from '../TextOnly/text-only.stories';
+import { checklist } from '../Checklist/checklist.stories';
+
+const layouts = {
+  Even: '50-50',
+  Offset: '60-40'
+};
+
+export const Default = () => {
+  const layout = select('Layout', layouts, layouts.Split);
+  const heading = text('Heading', '');
+  const showCta = boolean('CTA', false);
+
+  return `
+  <div class="tco-container-wrapper">
+    <div class="tco-container">
+      <div class="tco-columns tco-columns">
+        <div class="tco-column tco-column--${layout}">${textOnly()}</div>
+        <div class="tco-column tco-column--${layout}">${checklist()}</div>
+      </div>
+    </div>
+  </div>`;
+};
+
+export default {
+  title: 'Components / Columns'
+};
