4ad631edd25e644280b1719a82008520fbd381e7
Andy Knapp <andy.knapp@thinkcompany.com>
2021-02-19 10:59:45 -0500

    TCR-91: footer update (#81)
    * site footer curve and bubble
    
    * add space between bubble and content
    
    * remove emoji
    
    * change span to p>
    
    * replace span with p

--- src/components/SocialIcons/social-icons.stories.js
+++ src/components/SocialIcons/social-icons.stories.js
@@ -2,9 +2,6 @@ import { select, boolean } from '@storybook/addon-knobs';
 
 const colors = {
   Default: '',
-  Tan: 'tco-container-wrapper--tan',
-  Blue: 'tco-container-wrapper--blue',
-  Navy: 'tco-container-wrapper--navy',
   Glass: 'tco-container-wrapper--glass'
 };
 
@@ -53,6 +50,55 @@ export const socialLinks = () => {
   `;
 };
 
+export const socialLinksSmall = () => {
+  const iconSet = [];
+  const icons = [
+    {
+      name: 'linkedin',
+      include: boolean('Linkedin', true)
+    },
+    {
+      name: 'twitter',
+      include: boolean('Twitter', true)
+    },
+    {
+      name: 'instagram',
+      include: boolean('Instagram', true)
+    },
+    {
+      name: 'youtube',
+      include: boolean('Youtube', true)
+    },
+    {
+      name: 'facebook',
+      include: boolean('Facebook', true)
+    }
+  ];
+
+  icons.forEach(item => {
+    if (item.include) {
+      iconSet.push(`
+        <li>
+          <a class="tco-social-icons-link" href="#" target="_blank" rel="noopener noreferrer">
+            <svg class="tco-icon tco-icon--${item.name}" title="${item.name}">
+              <use xlink:href="/img/icons.svg#icon-${item.name}-small"></use>
+            </svg>
+            <span class="tco-accessibly-hidden">${item.name}</span>
+          </a>
+        </li>
+      `);
+    }
+  });
+
+  return `
+    <div class="tco-social-icons">
+      <ul class="tco-social-icons-list tco-social-icons-list--small">
+        ${iconSet.join('')}
+      </ul>
+    </div>
+  `;
+};
+
 export const WithContainer = () => {
   const containerColor = select('Background Color', colors, colors.Default);
 
@@ -60,6 +106,10 @@ export const WithContainer = () => {
     <div class="tco-container-wrapper ${containerColor}">
       <div class="tco-container">
         ${socialLinks()}
+      </div>
+
+      <div class="tco-container">
+        ${socialLinksSmall()}
       </div>
     </div>
   `;
