c1873f88b3967a2f96fa111f40d9dd56746b9e98
Andy Knapp <andy.knapp@thinkcompany.com>
2021-07-13 13:37:45 -0400

    TCR-254: Event speaker component (#118)
    * new layout for event speakers
    
    * shave eyebrow

--- src/components/Bio/Bio.stories.js
+++ src/components/Bio/Bio.stories.js
@@ -1,7 +1,7 @@
 import { boolean, text } from '@storybook/addon-knobs';
 import { socialLinks } from '../SocialIcons/social-icons.stories';
 
-export const Default = () => {
+export const author = () => {
   const eyebrow = text('Eyebrow', 'Meet the Author');
   const authorName = text('Author name', 'Abby DePrimo');
   const authorTitle = text('Author title', 'Vice President, Design Operations');
@@ -43,6 +43,40 @@ export const Default = () => {
   `;
 };
 
+export const speaker = () => {
+  const eyebrow = text('Eyebrow', 'About the Speaker');
+  const authorName = text('Speaker name', 'Abby DePrimo');
+  const authorTitle = text(
+    'Speaker title',
+    'Vice President, Design Operations'
+  );
+  const authorImage = text(
+    'Speaker image',
+    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg'
+  );
+  const authorBio = text(
+    'Speaker bio',
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel fermentum orci. In commodo tellus eget risus condimentum cursus. Maecenas cursus, risus tristique dictum venenatis, lectus quam finibus est, sit amet feugiat lacus nibh in urna.'
+  );
+
+  return `
+  <div class="tco-container-wrapper">
+	  <div class="tco-container">
+      <div class="tco-text-media tco-text-media--align-left tco-speaker">
+        <div class="tco-text-media-content tco-text-media-content-text">
+          <h2 class="tco-author-heading">${authorName}</h2>
+          <p class="tco-type-body--large">${authorTitle}</p>
+          <p class="tco-type-body--default">${authorBio}</p>
+        </div>
+        <div class="tco-text-media-content tco-text-media-content-media">
+          <img class="tco-speaker-image" src="${authorImage}" alt="${authorName}" />
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+};
+
 export default {
-  title: 'Components / Author'
+  title: 'Components / Bio'
 };
