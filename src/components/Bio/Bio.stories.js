f3e89dbf6164988416975dab31a51c98ff9db017
andy knapp <andy.knapp@thinkcompany.com>
2021-08-18 11:54:57 -0400

    bio controls added

--- src/components/Bio/Bio.stories.js
+++ src/components/Bio/Bio.stories.js
@@ -1,20 +1,7 @@
-import { boolean, text } from '@storybook/addon-knobs';
 import { socialLinks } from '../SocialIcons/social-icons.stories';
 
-export const author = () => {
-  const eyebrow = text('Eyebrow', 'Meet the Author');
-  const authorName = text('Author name', 'Abby DePrimo');
-  const authorTitle = text('Author title', 'Vice President, Design Operations');
-  const authorImage = text(
-    'Author image',
-    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg'
-  );
-  const authorBio = text(
-    'Author bio',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel fermentum orci. In commodo tellus eget risus condimentum cursus. Maecenas cursus, risus tristique dictum venenatis, lectus quam finibus est, sit amet feugiat lacus nibh in urna. Aliquam fringilla egestas augue, at dictum ex consequat et. Vestibulum ac imperdiet lectus. Integer rhoncus quis est a fringilla.'
-  );
-  const socialLabel = text('Social label', 'Say hello');
-  const showSocial = boolean('Show social links', true);
+const AuthorTemplate = args => {
+  const { eyebrow, name, title, image, bio, socialLabel, showSocial } = args;
 
   return `
   <div class="tco-container-wrapper">
@@ -22,9 +9,9 @@ export const author = () => {
       <div class="tco-text-media tco-text-media--align-right tco-author">
         <div class="tco-text-media-content tco-text-media-content-text">
           <span class="tco-author-eyebrow">${eyebrow}</span>
-          <h2 class="tco-author-heading">${authorName}</h2>
-          <p class="tco-author-title">${authorTitle}</p>
-          <p class="tco-author-bio tco-type-body--default">${authorBio}</p>
+          <h2 class="tco-author-heading">${name}</h2>
+          <p class="tco-author-title">${title}</p>
+          <p class="tco-author-bio tco-type-body--default">${bio}</p>
           ${
             showSocial
               ? `<div class="tco-author-social">
@@ -35,7 +22,7 @@ export const author = () => {
           }
         </div>
         <div class="tco-text-media-content tco-text-media-content-media tco-author-container-media">
-          <img class="tco-author-image" src="${authorImage}" alt="${authorName}" />
+          <img class="tco-author-image" src="${image}" alt="${name}" />
         </div>
       </div>
     </div>
@@ -43,43 +30,18 @@ export const author = () => {
   `;
 };
 
-export const speaker = () => {
-  const authorName = text('Speaker name', 'Abby DePrimo');
-  const authorTitle = text(
-    'Speaker title',
-    'Vice President, Design Operations'
-  );
-  const authorImage = text(
-    'Speaker image',
-    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg'
-  );
-  const authorBio = text(
-    'Speaker bio',
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel fermentum orci. In commodo tellus eget risus condimentum cursus. Maecenas cursus, risus tristique dictum venenatis, lectus quam finibus est, sit amet feugiat lacus nibh in urna.'
-  );
+export const Author = AuthorTemplate.bind({});
 
-  return `
-  <div class="tco-container-wrapper">
-	  <div class="tco-container">
-      <div class="tco-text-media tco-text-media--align-left tco-speaker">
-        <div class="tco-text-media-content tco-text-media-content-text tco-text-media-content-text--has-image">
-          <h2 class="tco-author-heading">${authorName}</h2>
-          <p class="tco-type-body--large">${authorTitle}</p>
-          <p class="tco-type-body--default">${authorBio}</p>
-        </div>
-        <div class="tco-text-media-content tco-text-media-content-media">
-          <img class="tco-speaker-image" src="${authorImage}" alt="${authorName}" />
-        </div>
-      </div>
-    </div>
-  </div>
-  `;
+Author.args = {
+  eyebrow: 'Meet the Author',
+  bio:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel fermentum orci. In commodo tellus eget risus condimentum cursus. Maecenas cursus, risus tristique dictum venenatis, lectus quam finibus est, sit amet feugiat lacus nibh in urna. Aliquam fringilla egestas augue, at dictum ex consequat et. Vestibulum ac imperdiet lectus. Integer rhoncus quis est a fringilla.',
+  socialLabel: 'Say Hello',
+  showSocial: true
 };
 
-const Template = args => {
-  const { name, title, image } = args;
-  const bio =
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel fermentum orci. In commodo tellus eget risus condimentum cursus. Maecenas cursus, risus tristique dictum venenatis, lectus quam finibus est, sit amet feugiat lacus nibh in urna.';
+const SpeakerTemplate = args => {
+  const { name, title, image, bio } = args;
 
   return `
     <div class="tco-container-wrapper">
@@ -99,26 +61,19 @@ const Template = args => {
   `;
 };
 
-export const TestBio = Template.bind({});
+export const Speaker = SpeakerTemplate.bind({});
 
-TestBio.args = {
-  name: 'Abby DePrimo',
-  title: 'Vice President, Design Operations',
-  image:
-    'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg'
+Speaker.args = {
+  bio:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel fermentum orci. In commodo tellus eget risus condimentum cursus. Maecenas cursus, risus tristique dictum venenatis, lectus quam finibus est, sit amet feugiat lacus nibh in urna.'
 };
 
 export default {
   title: 'Components / Bio',
-  argTypes: {
-    name: {
-      control: 'text'
-    },
-    title: {
-      control: 'text'
-    },
-    image: {
-      control: 'text'
-    }
+  args: {
+    name: 'Abby DePrimo',
+    title: 'Vice President, Design Operations',
+    image:
+      'https://3vwizk2qtr8l3diwrm3r2ba0-wpengine.netdna-ssl.com/wp-content/uploads/2020/03/AbbyDePrimo_Web.jpg'
   }
 };
