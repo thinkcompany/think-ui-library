5fd86b48ef3419dd803e81fdde8073a8c3d57f14
andy knapp <andy.knapp@thinkcompany.com>
2021-08-26 16:41:56 -0400

    build out checkmark style options

--- src/components/Checklist/checklist.stories.js
+++ src/components/Checklist/checklist.stories.js
@@ -1,11 +1,21 @@
+const checkOptions = ['default', 'alt', 'none'];
+
 const ChecklistTemplate = args => {
-  const { eyebrow } = args;
+  const { eyebrow, checkType } = args;
   const items = [
     'Launch a new product',
     'Transform an existing product',
     'Maintain and improve business systems',
     'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vitae dui posuere'
   ];
+
+  let checkMarkClass = '';
+
+  if (checkType === 'alt') {
+    checkMarkClass = 'tco-checklist--alt-checkmark';
+  } else if (checkType === 'none') {
+    checkMarkClass = 'tco-checklist--no-checkmark';
+  }
 
   return `
   <div class="tco-container-wrapper">
@@ -18,7 +28,7 @@ const ChecklistTemplate = args => {
           `
             : ''
         }
-        <ul>
+        <ul class="${checkMarkClass}">
           ${items.map(item => `<li>${item}</li>`).join('')}
         </ul>
       </div>
@@ -32,6 +42,14 @@ export const Checklist = ChecklistTemplate.bind({});
 export default {
   title: 'Components / Checklist',
   args: {
-    eyebrow: 'How we can help'
+    eyebrow: 'How we can help',
+    checkType: checkOptions[0]
+  },
+  argTypes: {
+    checkType: {
+      name: 'checkmark type',
+      control: 'inline-radio',
+      options: checkOptions
+    }
   }
 };
