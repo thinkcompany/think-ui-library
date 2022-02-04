13edccf7b0ffaef0d7d4d7ac1071fe2bcd2421d7
John Young <john.young@thinkcompany.com>
2022-02-04 14:17:03 -0500

    Feature/swim 1449/add google analytics (#155)
    * Commit results of running npm install @storybook/addon-google-analytics
    * Add GA instrumentation

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -2,6 +2,9 @@ import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
 import thinkTheme from './thinkTheme';
 
+window.STORYBOOK_GA_ID = "UA-5790823-8"
+window.STORYBOOK_REACT_GA_OPTIONS = {}
+
 addons.setConfig({
   theme: {
     ...themes.light,
