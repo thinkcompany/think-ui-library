a95297b91e9815ef86568eaf2f9f430f878ab768
andy knapp <andy.knapp@thinkcompany.com>
2022-02-04 14:27:46 -0500

    tidy up

--- src/components/CaseStudyCTA/CaseStudyCTA.stories.js
+++ src/components/CaseStudyCTA/CaseStudyCTA.stories.js
@@ -157,7 +157,7 @@ const caseStudyCard = (input, cardtype) => {
           }
           <p class="tco-text-link tco-link--dark-theme">
             ${input.cta}
-            <svg class="tco-text-link-icon" width="20" height="20" viewBox="0 0 20 20" role="presentation">
+            <svg class="tco-text-link-icon tco-text-link-icon--alt" width="20" height="20" viewBox="0 0 20 20" role="presentation">
               <use xlink:href="/img/icons.svg#icon-arrow-right"></use>
             </svg>
           </p>
