276c1ba48009101af0a58bc4a4c4b360528854c5
Andy Knapp <andy.knapp@thinkcompany.com>
2022-12-09 12:43:46 -0500

    SWIM-2744: Image Panel Component (#177)

---
+++ src/components/ImagePanel/ImagePanel.stories.js
@@ -0,0 +1,97 @@
+const ImagePanelTemplate = () => {
+  const imageItem = i => {
+    const images = [
+      {
+        source: 'https://dummyimage.com/489x326/0062c3',
+        class: 'tco-panel-item--a',
+        width: 489,
+        height: 326
+      },
+      {
+        source: 'https://dummyimage.com/489x326/0062c3',
+        class: 'tco-panel-item--b',
+        width: 489,
+        height: 346
+      },
+      {
+        source: 'https://dummyimage.com/178x386/0062c3',
+        class: 'tco-panel-item--c',
+        width: 178,
+        height: 386
+      },
+      {
+        source: 'https://dummyimage.com/489x326/0062c3',
+        class: 'tco-panel-item--d',
+        width: 489,
+        height: 326
+      },
+      {
+        source: 'https://dummyimage.com/489x326/0062c3',
+        class: 'tco-panel-item--e',
+        width: 489,
+        height: 326
+      },
+      {
+        source: 'https://dummyimage.com/178x386/0062c3',
+        class: 'tco-panel-item--f',
+        width: 178,
+        height: 386
+      },
+      {
+        source: 'https://dummyimage.com/489x326/0062c3',
+        class: 'tco-panel-item--g',
+        width: 489,
+        height: 326
+      },
+      {
+        source: 'https://dummyimage.com/178x386/0062c3',
+        class: 'tco-panel-item--h',
+        width: 178,
+        height: 386
+      },
+      {
+        source: 'https://dummyimage.com/489x326/0062c3',
+        class: 'tco-panel-item--i',
+        width: 489,
+        height: 326
+      }
+    ];
+
+    let image = images[i];
+
+    return `
+      <figure class="tco-panel-item ${image.class}" style="--width:${image.width} ">
+        <img class="tco-panel-image" alt="A placeholder image" src="${image.source}" width="${image.width}" height="${image.height}" />
+      </figure>
+    `;
+  };
+
+  const imageItems = () => {
+    let output = '';
+
+    for (let index = 0; index < 9; index++) {
+      output += imageItem(index);
+    }
+
+    return output;
+  };
+
+  return `<div class="tco-image-panel">${imageItems()}</div>`;
+};
+
+export const ImagePanel = ImagePanelTemplate.bind({});
+
+export default {
+  title: 'Components / Image Panel',
+  parameters: {
+    component: ImagePanel,
+    componentSubtitle: 'The Image Panel is used on the homepage.',
+    controls: { hideNoControlsWarning: true },
+    docs: {
+      description: {
+        component:
+          'There are two image sizes / aspect ratios<br> 1) <strong>aspect ratio </strong>6:4 | <strong>max display size</strong> 489 x 326 | <strong>upload size</strong> 734 x 489 <br>12 <strong>aspect ratio </strong>89:193 | <strong>max display size</strong> 178 x 386 | <strong>upload size</strong> 267 x 579'
+      }
+    }
+  }
+};
