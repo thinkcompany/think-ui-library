5773e8f450720fafca6c67a21bf824b001b91e36
andy <andy.knapp@thinkcompany.com>
2022-12-15 17:17:14 -0500

    refine carousel card

--- src/components/Card/motion-card.js
+++ src/components/Card/motion-card.js
@@ -18,40 +18,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardDuration = 1000;
     const cardCount = Math.round(trackWidth / miniWidth);
     const keyCount = cardCount / 2 + 1;
-    let keyframes = [];
-
-    const trackTiming = {
-      duration: cardDuration * cardCount,
-      iterations: Infinity
-    };
-    const observerOptions = {
-      root: card,
-      rootMargin: '0px 0px 0px -20%',
-      threshold: 0.2
-    };
 
     const initialOffset = () => {
       const offset = miniWidth - (cardWidth - miniWidth) / 2;
       offsetContainer.style.transform = 'translateX(-' + offset + 'px)';
       controlContainer.style.transform = 'translateX(' + offset + 'px)';
     };
-
-    const observerCallback = entries => {
-      entries.forEach(entry => {
-        const { target } = entry;
-        // if ( entry.intersectionRatio >= observerOptions.threshold ) {
-        if (entry.isIntersecting) {
-          target.classList.add(miniClass);
-        } else {
-          target.classList.remove(miniClass);
-        }
-      });
-    };
-
-    const observer = new IntersectionObserver(
-      observerCallback,
-      observerOptions
-    );
 
     const doClones = () => {
       minis.forEach(mini => {
@@ -61,15 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     };
 
-    const observeSubjects = () => {
-      let subjects = [...track.querySelectorAll('.tco-mini-card')];
+    const trackAnimation = () => {
+      let keyframes = [];
 
-      subjects.forEach(subject => {
-        observer.observe(subject);
-      });
-    };
-
-    const buildKeyframes = () => {
+      //  build keyframes
       for (let i = 0; i < keyCount; i++) {
         let miniW = miniWidth * i;
 
@@ -78,38 +45,78 @@ document.addEventListener('DOMContentLoaded', () => {
           { transform: 'translateX(-' + miniW + 'px)' }
         );
       }
+
+      console.log(keyframes);
+
+      const trackTiming = {
+        duration: cardDuration * cardCount,
+        iterations: Infinity
+      };
+
+      const animateTrack = track.animate(keyframes, trackTiming);
+
+      animateTrack.pause();
+
+      const toggleAnimation = () => {
+        if (animateTrack.playState === 'paused') {
+          animateTrack.play();
+          track.classList.remove(pauseClass);
+          control.classList.remove(pauseClass);
+        } else {
+          animateTrack.pause();
+          track.classList.add(pauseClass);
+          control.classList.add(pauseClass);
+        }
+      };
+      control.addEventListener('click', toggleAnimation);
     };
 
-    const toggleAnimation = () => {
-      if (animateTrack.playState === 'paused') {
-        animateTrack.play();
-        track.classList.remove(pauseClass);
-        control.classList.remove(pauseClass);
-      } else {
-        animateTrack.pause();
-        track.classList.add(pauseClass);
-        control.classList.add(pauseClass);
-      }
-    };
+    const animateCard = () => {
+      const cards = track.querySelectorAll('.tco-mini-card');
+      const observerOptions = {
+        root: card,
+        rootMargin: '0% -33% 0% -33%',
+        threshold: [0, 1]
+      };
 
-    const doAnimation = () => {
-      if (animateTrack.effect.getComputedTiming().progress > 0.3) {
-        console.log('now');
-      }
+      const observerCallback = entries => {
+        entries.forEach(entry => {
+          const cardKeyframes = [
+            { transform: 'scale(1)' },
+            { transform: 'scale(1.2)' },
+            { transform: 'scale(1)' }
+          ];
+          const cardTiming = {
+            duration: cardDuration,
+            iterations: 1
+          };
+
+          const { target } = entry;
+          console.log(target);
+          // if ( entry.intersectionRatio >= observerOptions.threshold ) {
+          if (entry.isIntersecting) {
+            target.classList.add(miniClass);
+            target.animate(cardKeyframes, cardTiming);
+          } else {
+            target.classList.remove(miniClass);
+          }
+        });
+      };
+
+      const observer = new IntersectionObserver(
+        observerCallback,
+        observerOptions
+      );
+
+      cards.forEach((card, i) => {
+        observer.observe(card);
+      });
     };
 
     initialOffset();
     doClones();
-    buildKeyframes();
-    observeSubjects();
-
-    const animateTrack = track.animate(keyframes, trackTiming);
-
-    animateTrack.pause();
-
-    doAnimation();
-
-    control.addEventListener('click', toggleAnimation);
+    trackAnimation();
+    animateCard();
   };
 
   const sliderControls = card => {
@@ -121,70 +128,65 @@ document.addEventListener('DOMContentLoaded', () => {
     const control = controlContainer.querySelector('.tco-motion-control');
     const miniWidth = 174;
     const pauseClass = 'tco-motion-track--paused';
+    let rowWidths = [];
+    let widestRow = '100%';
 
-    const animateRows = () => {
-      let rowWidths = [];
-      let widestRow = '100%';
+    slideRows.forEach((row, i) => {
+      let keyframes = [];
+      let ogSlides = row.querySelectorAll('.tco-mini-slide');
+      let rowWidth =
+        miniWidth * (ogSlides.length * 2) + (ogSlides.length - 1) * 8; // accounts for gap
+      rowWidths.push(rowWidth); // will be used to set width of parent container
 
-      slideRows.forEach((row, i) => {
-        let keyframes = [];
-        let ogSlides = row.querySelectorAll('.tco-mini-slide');
-        let rowWidth =
-          miniWidth * (ogSlides.length * 2) + (ogSlides.length - 1) * 8; // accounts for gap
-        rowWidths.push(rowWidth); // will be used to set width of parent container
-
-        // clone the images in each row
-        ogSlides.forEach(ogSlide => {
-          let clone = ogSlide.cloneNode(true);
-          clone.classList.add('tco-mini-slide--clone');
-          row.append(clone);
-        });
-
-        // now get all the slides
-        const slides = row.querySelectorAll('.tco-mini-slide');
-        const slideCount = slides.length / 2 + 1;
-        // we only need to build keyframes for 1/2 (+1) of all slides (og and clones) for a seamless loop
-        for (let i = 0; i < slideCount; i++) {
-          let miniW = miniWidth + 8;
-          miniW = miniW * i;
-
-          keyframes.push({ transform: 'translateX(-' + miniW + 'px)' });
-        }
-
-        // set up the timing object
-        let trackTiming = {
-          duration: 4000 * slides.length,
-          iterations: Infinity
-        };
-
-        if (i != 0) {
-          trackTiming = {
-            duration: 3000 * slides.length,
-            iterations: Infinity
-          };
-        }
-
-        const animateRow = row.animate(keyframes, trackTiming);
-        animateRow.pause();
-
-        const toggleAnimation = () => {
-          if (animateRow.playState === 'paused') {
-            animateRow.play();
-            control.classList.remove(pauseClass);
-          } else {
-            animateRow.pause();
-            control.classList.add(pauseClass);
-          }
-        };
-
-        control.addEventListener('click', toggleAnimation);
+      // clone the images in each row
+      ogSlides.forEach(ogSlide => {
+        let clone = ogSlide.cloneNode(true);
+        clone.classList.add('tco-mini-slide--clone');
+        row.append(clone);
       });
 
-      widestRow = rowWidths.reduce((a, b) => Math.max(a, b), -Infinity) + 'px'; // use widest row to set parent width
-      track.style.width = widestRow;
-    };
+      // now get all the slides
+      const slides = row.querySelectorAll('.tco-mini-slide');
+      const slideCount = slides.length / 2 + 1;
+      // we only need to build keyframes for 1/2 (+1) of all slides (og and clones) for a seamless loop
+      for (let i = 0; i < slideCount; i++) {
+        let miniW = miniWidth + 8;
+        miniW = miniW * i;
 
-    animateRows();
+        keyframes.push({ transform: 'translateX(-' + miniW + 'px)' });
+      }
+
+      // set up the timing object
+      let trackTiming = {
+        duration: 4000 * slides.length,
+        iterations: Infinity
+      };
+
+      if (i != 0) {
+        trackTiming = {
+          duration: 3000 * slides.length,
+          iterations: Infinity
+        };
+      }
+
+      const animateRow = row.animate(keyframes, trackTiming);
+      animateRow.pause();
+
+      const toggleAnimation = () => {
+        if (animateRow.playState === 'paused') {
+          animateRow.play();
+          control.classList.remove(pauseClass);
+        } else {
+          animateRow.pause();
+          control.classList.add(pauseClass);
+        }
+      };
+
+      control.addEventListener('click', toggleAnimation);
+    });
+
+    widestRow = rowWidths.reduce((a, b) => Math.max(a, b), -Infinity) + 'px'; // use widest row to set parent width
+    track.style.width = widestRow;
   };
 
   carouselControls(carouselCard);
