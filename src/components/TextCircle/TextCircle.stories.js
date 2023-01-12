8d77ddbaf244e3b55573db4eb7201ba44a05df4e
andy <andy.knapp@thinkcompany.com>
2023-01-12 09:12:03 -0500

    remove period and adjust circle position

--- src/components/TextCircle/TextCircle.stories.js
+++ src/components/TextCircle/TextCircle.stories.js
@@ -15,7 +15,7 @@ const TextCircleTemplate = args => {
 export const TextCircle = TextCircleTemplate.bind({});
 
 TextCircle.args = {
-  text: 'Not just clients &#8212;</br><span class="tco-type-treatment--circle">partners.<svg viewbox="0 0 501 154" fill="none"><path d="M289.5 132c-151 27-283.433 2.741-287.015-34.718C-1.096 59.825 107.105 18.835 244.16 5.73c137.054-13.105 251.063 6.637 254.644 44.095 3.281 34.313-128.312 86.31-248.881 102.402" pathLength="1" /></svg></span>'
+  text: 'Not just clients&#8212;</br><span class="tco-type-treatment--circle">partners<svg viewbox="0 0 501 154" fill="none"><path d="M289.5 132c-151 27-283.433 2.741-287.015-34.718C-1.096 59.825 107.105 18.835 244.16 5.73c137.054-13.105 251.063 6.637 254.644 44.095 3.281 34.313-128.312 86.31-248.881 102.402" pathLength="1" /></svg></span>'
 };
 
 export default {
