a3d6b83ddec5c6363e49be2799a2b7287b28606c
andy <andy.knapp@thinkcompany.com>
2023-01-24 10:10:36 -0500

    connector left arrow animation

--- src/components/Arrows/Arrows.stories.js
+++ src/components/Arrows/Arrows.stories.js
@@ -16,14 +16,39 @@ const FullSectionRightTemplate = args => {
 
 export const FullSectionRight = FullSectionRightTemplate.bind({});
 
-FullSectionRight.parameters = {
-  controls: { hideNoControlsWarning: true }
+const ConnectorLeftTemplate = args => {
+  return `
+  <div class="tco-container-wrapper tco-container-wrapper--glass tco-container-wrapper--bleed-bottom">
+    <div class="tco-container">
+      <div class="tco-image-only tco-image-only--offset-none tco-image-only--image">
+        <img
+          class="tco-image-only-image"
+          alt="A brief description of the image"
+          src="https://www.thinkcompany.com/wp-content/uploads/2021/01/careers-header.jpg"
+        />
+        <svg class="tco-arrow tco-arrow--connector-left active" viewbox="0 0 230 335" fill="none"><path d="M119.997 2.497C-53.501 71.997-2.003 186 71.998 197.5c62.515 9.715 114 15.999 147.43 126.071-10.29-7.4-28.95-20.139-21.26-11.894 9.612 10.306 24.024 22.706 28 19.65 3.181-2.445-1.965-32.412-4.935-47.09" pathLength="1" /></svg>
+      </div>
+    </div>
+  </div>
+  <div class="tco-container-wrapper tco-container-wrapper--glass">
+    <div class="tco-container tco-container--center tco-container--mid" style="padding-top:min(34vw, 290px);">
+      <div class="tco-text-only tco-text-only--center  ">
+        <h2 class="tco-text-only-heading tco-type-display--sans">Our growing team of over 150 Thinkers can tackle some of your greatest challenges.</h2>
+        <div class="tco-text-only-content tco-text-only-content--center tco-container-- tco-type-body--large">Chances are, you’ll really like working with us.</div>
+        <a href="/" class="tco-btn tco-btn--primary tco-btn--space-xl tco-animation-trigger">Replay animation</a>
+      </div>
+    </div>
+  </div>
+  `;
 };
+
+export const ConnectorLeft = ConnectorLeftTemplate.bind({});
 
 export default {
   title: 'Motion / Arrows',
-  component: [FullSectionRight],
+  component: [FullSectionRight, ConnectorLeftTemplate],
   parameters: {
-    component: FullSectionRight
+    component: FullSectionRight,
+    controls: { hideNoControlsWarning: true }
   }
 };
