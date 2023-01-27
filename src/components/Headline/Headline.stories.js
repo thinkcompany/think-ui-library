6bf33b6562376c748e4d84b8e29a760bada00f7e
andy <andy.knapp@thinkcompany.com>
2023-01-27 16:54:54 -0500

    headline animation markup

--- src/components/Headline/Headline.stories.js
+++ src/components/Headline/Headline.stories.js
@@ -1,13 +1,14 @@
 const HeadlineTemplate = args => {
   return `
-  <div class="tco-container-wrapper tco-text-only-page-header">
-	  <div class="tco-container">
-      <div class="tco-text-only ">
-        <h1 class="tco-text-only-heading">
-          <span>World-class</span>
-          <span>digital experiences</span>
-          <span>for every user</span>
+  <div class="tco-container-wrapper">
+	  <div class="tco-container tco-container--default tco-page-header-big-bubble">
+      <div class="tco-text-only">
+        <h1 class="tco-text-only-heading tco-motion-headline">
+          <span class="tco-headline-fragment tco-headline-fragment--a">World-class</span>
+          <span class="tco-headline-fragment tco-headline-fragment--b">digital experiences</span>
+          <span class="tco-headline-fragment tco-headline-fragment--c">for every user</span>
         </h1>
+        <a href="/" class="tco-btn tco-btn--primary tco-btn--space-xl">Start a project</a>
       </div>
     </div>
   </div>
