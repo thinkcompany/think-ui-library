3cf65b4087d7ca4b9b8fd5d35f7d707556229783
andy <andy.knapp@thinkcompany.com>
2023-02-07 16:11:12 -0500

    reset animation when theme toggle switched

--- src/components/Headline/headline.js
+++ src/components/Headline/headline.js
@@ -2,26 +2,49 @@ document.addEventListener('DOMContentLoaded', () => {
   const fragments = document.querySelectorAll('.tco-headline-fragment');
   const blurbs = document.querySelectorAll('.tco-headline-statement');
   const blurbContainer = document.querySelector('.tco-headline-animation');
+  const toggle = document.querySelector('.tco-theme-toggle');
   const prefersReduced =
     window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
     window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
 
-  const playHeadlineAnimation = () => {
-    fragments.forEach(fragment => {
-      fragment.getAnimations().forEach(animation => {
-        animation.play();
+  const playHeadlineAnimation = state => {
+    if (state === 'play') {
+      fragments.forEach(fragment => {
+        fragment.getAnimations().forEach(animation => {
+          animation.play();
+        });
       });
-    });
 
-    blurbs.forEach(blurb => {
-      blurb.getAnimations().forEach(animation => {
-        animation.play();
+      blurbs.forEach(blurb => {
+        blurb.getAnimations().forEach(animation => {
+          animation.play();
+        });
       });
-    });
+    } else {
+      fragments.forEach(fragment => {
+        fragment.getAnimations().forEach(animation => {
+          animation.cancel();
+          animation.play();
+        });
+      });
+
+      blurbs.forEach(blurb => {
+        blurb.getAnimations().forEach(animation => {
+          animation.cancel();
+          animation.play();
+        });
+      });
+    }
   };
 
   if (!prefersReduced) {
-    setTimeout(playHeadlineAnimation, 1000);
+    setTimeout(playHeadlineAnimation('play'), 1000);
+
+    if (toggle) {
+      toggle.addEventListener('click', () => {
+        setTimeout(playHeadlineAnimation('restart'), 1000);
+      });
+    }
   } else {
     blurbContainer.classList.add('tco-prefers-reduced');
   }
