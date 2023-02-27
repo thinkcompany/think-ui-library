3f13a18f690d21bb215a1e39f8d9e2731238f2cb
andy <andy.knapp@thinkcompany.com>
2023-02-27 17:07:18 -0500

    accordion component

---
+++ src/components/Accordion/accordion.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const accordionButtons = document.querySelectorAll('.tco-accordion-button');
+
+  const toggleAccordion = () => {
+    accordionButtons.forEach(button => {
+      button.addEventListener('click', event => {
+        event.stopImmediatePropagation();
+
+        const toggleState = button.getAttribute('aria-expanded');
+
+        if (toggleState === 'true') {
+          button.setAttribute('aria-expanded', 'false');
+          button.classList.remove('tco-accordion-button--active');
+        } else if (toggleState === 'false') {
+          button.setAttribute('aria-expanded', 'true');
+          button.classList.add('tco-accordion-button--active');
+        }
+      });
+    });
+  };
+
+  if (accordionButtons) {
+    toggleAccordion();
+  }
+});
