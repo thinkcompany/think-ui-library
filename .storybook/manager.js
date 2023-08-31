b72ef030ae69b648529b1962f4a87087db088c4d
andy <andy.knapp@thinkcompany.com>
2023-08-31 15:05:58 -0400

    clean up manager file

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,13 +1,2 @@
-import { addons } from '@storybook/addons';
-import { themes } from '@storybook/theming';
-import thinkTheme from './thinkTheme';
-
 window.STORYBOOK_GA_ID = "UA-5790823-8"
 window.STORYBOOK_REACT_GA_OPTIONS = {}
-
-addons.setConfig({
-  theme: {
-    ...themes.light,
-    ...thinkTheme
-  }
-});
