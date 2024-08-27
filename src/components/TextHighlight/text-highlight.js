6388d4c00aeae741c8e27ede4df2ca5b7d748a67
andy <andy.knapp@thinkcompany.com>
2024-08-27 11:08:28 -0400

    fix linting error

--- src/components/TextHighlight/text-highlight.js
+++ src/components/TextHighlight/text-highlight.js
@@ -1,15 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   const textHighlight = document.querySelector('.tco-type-treatment--highlight');
+  // const textHighlightWords = textHighlight.querySelectorAll('span');
 
   const observerOptions = {
     rootMargin: '0% 0% -40% 0%',
     threshold: 1
   };
 
+  if (textHighlight) {
+    const text = textHighlight.textContent;
+    const wrappedText = text
+      .split(' ')
+      .map(word => `<span>${word}</span>`)
+      .join(' ');
+
+    textHighlight.innerHTML = wrappedText; // Update innerHTML here
+  }
+
   const observerCallback = entries => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         textHighlight.classList.add('active');
+        const spans = textHighlight.querySelectorAll('span');
+
+        spans.forEach((span, index) => {
+          setTimeout(() => {
+            span.classList.add('active');
+          }, index * 40);
+        });
       }
     });
   };
