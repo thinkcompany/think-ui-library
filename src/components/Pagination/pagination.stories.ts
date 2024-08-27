f74062ab4e5cd3874174a033a54a7ba71318eac1
andy <andy.knapp@thinkcompany.com>
2024-08-27 14:04:57 -0400

    update pagination and pull quote component dark mode styles

--- src/components/Pagination/pagination.stories.ts
+++ src/components/Pagination/pagination.stories.ts
@@ -5,8 +5,8 @@ export const Default = () => {
       <nav class="tco-pagination" role="navigation">
         <ul class="tco-pagination-list">
           <li class="tco-pagination-list-item tco-pagination-list-item--prev">
-            <a class="tco-pagination-link" href="#">
-              <svg class="tco-icon" width="16" height="16" viewBox="0 0 16 16">
+            <a href="" class="tco-link--dark-theme tco-pagination-link">
+              <svg class="tco-icon tco-text-link-icon--alt" width="16" height="16" viewBox="0 0 16 16">
                 <use xlink:href="/img/icons.svg#icon-arrow-right"></use>
               </svg>
               Previous
@@ -17,16 +17,16 @@ export const Default = () => {
           </li>
           <li class="tco-pagination-list-item"><span class="tco-pagination-page tco-pagination-page--ellipsis">…</span></li>
 
-          <li class="tco-pagination-list-item"><a class="tco-pagination-page" href="#">12</a></li>
-          <li class="tco-pagination-list-item"><a class="tco-pagination-page" href="#">13</a></li>
+          <li class="tco-pagination-list-item"><a class="tco-pagination-page tco-link--dark-theme" href="#">12</a></li>
+          <li class="tco-pagination-list-item"><a class="tco-pagination-page tco-link--dark-theme" href="#">13</a></li>
           <li class="tco-pagination-list-item tco-pagination-list-item--current">
             <span class="tco-pagination-page tco-pagination-page--current page-number page-numbers current">14</span>
           </li>
-          <li class="tco-pagination-list-item"><a class="tco-pagination-page" href="#">15</a></li>
-          <li class="tco-pagination-list-item"><a class="tco-pagination-page" href="#">16</a></li>
-          <li class="tco-pagination-list-item"><a class="tco-pagination-page" href="#">17</a></li>
+          <li class="tco-pagination-list-item"><a class="tco-pagination-page tco-link--dark-theme" href="#">15</a></li>
+          <li class="tco-pagination-list-item"><a class="tco-pagination-page tco-link--dark-theme" href="#">16</a></li>
+          <li class="tco-pagination-list-item"><a class="tco-pagination-page tco-link--dark-theme" href="#">17</a></li>
           <li class="tco-pagination-list-item"><span class="tco-pagination-page tco-pagination-page--ellipsis">…</span></li>
-          <li class="tco-pagination-list-item"><a class="tco-pagination-page" href="#/">28</a></li>
+          <li class="tco-pagination-list-item"><a class="tco-pagination-page tco-link--dark-theme" href="#/">28</a></li>
 
           <li class="tco-pagination-list-item tco-pagination-list-item--next">
             <a href="" class="tco-link--dark-theme tco-pagination-link">
